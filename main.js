8935786ec0a528a79774fd961d7fed4d0025347a
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-04-15 16:04:18 +0200

    add request fetch

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const getUsers = ()=> {
-  console.log('dziala');
+const getUsers = (e) => {
+  e.preventDefault();
 
-}
+  const usersNumber = document.querySelector('[name="users-number"]').value;
+  const usersGender = document.querySelector('[name="gender"]').value;
+  // console.log(usersNumber, usersGender);
+  const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${usersGender === 'both' ? 'male,female' : usersGender}`;
+  console.log(url);
+  fetch(url) // obietnica - stan oczekujacy (pending)
+    // obietnica - spelniona | odrzucona
+    // wykonuje sie then - pozytywnie
+    .then((response) => {
+      // console.log(response);
+      if (response.status !== 200) {
+        throw Error("to nie jest odpowiedz 200");
+      } else {
+        return response.json();
+      }
+    })
+    .then((json) => console.log(json))
+    // wykonuje gdy odrzucone
+    .catch((err) => console.log(err));
+};
 
-document.querySelector('button').addEventListener('click', getUsers)
+document.querySelector(".generator").addEventListener("submit", getUsers);
