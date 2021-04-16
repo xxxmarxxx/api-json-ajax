d0c786c99ed30d69beb29b710fab82e8667dc320
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-04-16 11:47:26 +0200

    add const showUsers

--- main.js
+++ main.js
@@ -1,25 +1,40 @@
 const getUsers = (e) => {
   e.preventDefault();
 
-  const usersNumber = document.querySelector('[name="users-number"]').value;
-  const usersGender = document.querySelector('[name="gender"]').value;
-  // console.log(usersNumber, usersGender);
-  const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${usersGender === 'both' ? 'male,female' : usersGender}`;
+  const usersNumber = document.querySelector('[name = "users-number"]').value;
+  const usersGender = document.querySelector('[name = "gender"]').value;
+  const url = `https://randomuser.me/api/?results=${usersNumber}&gender=${usersGender === "both" ? "male,female" : usersGender}`;
   console.log(url);
-  fetch(url) // obietnica - stan oczekujacy (pending)
-    // obietnica - spelniona | odrzucona
-    // wykonuje sie then - pozytywnie
-    .then((response) => {
+   fetch(url) //obietnica - oczekujący (pending)
+    // obietnica - rozstrzygnieta (spełnione | odrzucone)
+    //wykonuje się then - pozytywnie
+    .then(response => {
       // console.log(response);
       if (response.status !== 200) {
-        throw Error("to nie jest odpowiedz 200");
+        throw Error("To nie jest odpowedź 200")
       } else {
-        return response.json();
+        return response.json() //Fetch API = json() z body wyodrębnia json i parsuje na obiekt
       }
     })
-    .then((json) => console.log(json))
-    // wykonuje gdy odrzucone
-    .catch((err) => console.log(err));
+    .then(json => showUsers(json.results))
+    //wykonuje gdy rozstrzygnięcie - odrzucona
+    .catch(err => console.log(err))
+}
+
+const showUsers = (users) => {
+  const resultArea = document.querySelector('.user-list');
+  
+  // resultArea.textContent = '';
+  users.forEach((user) => {
+    const item = document.createElement("div");
+    item.className = "user";
+    item.innerHTML = `
+  <div class='user__name'>${user.name.title.toUpperCase()}${user.name.first.toUpperCase()}${user.name.last.toUpperCase()}
+  </div>
+  <img class="user__image" src=${user.picture.medium}>
+  `;
+   resultArea.appendChild(item)
+  });
 };
 
-document.querySelector(".generator").addEventListener("submit", getUsers);
+document.querySelector('.generator').addEventListener('submit', getUsers);
